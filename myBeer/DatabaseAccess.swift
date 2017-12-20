7012c10ba9e779c82d55294cb4d3e4c619741f37
CHANCEREL Loic <chancerl@mac04-m102.utt.fr>
2017-12-20 13:06:17 +0100

    Linking Database Framework

---
+++ myBeer/DatabaseAccess.swift
@@ -0,0 +1,16 @@
+//
+//  DatabaseAccess.swift
+//  myBeer
+//
+//  Created by CHANCEREL Loic on 20/12/2017.
+//  Copyright © 2017 CHANCEREL Loic. All rights reserved.
+//
+
+import UIKit
+import SQLite
+
+class DatabaseAccess {
+    
+    let db = try(Connection("))
+
+}
